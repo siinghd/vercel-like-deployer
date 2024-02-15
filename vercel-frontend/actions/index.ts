b18c1e778d9fe0e151f2c90ecd9e9a9863aff586
Harpreet Singh <szinghh@gmail.com>
2024-02-15 19:55:20 +0100

    added projectPath on slug

--- vercel-frontend/actions/index.ts
+++ vercel-frontend/actions/index.ts
@@ -8,7 +8,7 @@ export const handleFormAction = async (formdata: {
   runCmd: string;
   projectPath: string;
 }) => {
-  const response = await fetch('http://localhost:3001/deploy', {
+  const response = await fetch('https://api-deployer.hsingh.site/deploy', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
